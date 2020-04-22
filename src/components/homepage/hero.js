b3f9fcc37df35397fd2b6e9cde79803ac2a19e0f
truehello <trudelmarc@gmail.com>
2020-04-21 20:26:19 -0700

    added links to hero

--- src/components/homepage/hero.js
+++ src/components/homepage/hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import thoughtProcessIllustration from "../../images/thought_process.svg";
+import { Link } from "gatsby"
 
 const Hero = () => {
     return (
@@ -17,20 +18,20 @@ const Hero = () => {
         </p>
         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div className="rounded-md shadow">
-            <a
-              href="#"
+            <Link
+              to="/about"
               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
             >
               For Parents
-            </a>
+            </Link>
           </div>
           <div className="mt-3 sm:mt-0 sm:ml-3">
-            <a
-              href="#"
+            <Link
+              to="/about"
               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
             >
               For Professionals
-            </a>
+            </Link>
           </div>
         </div>
       </div>
